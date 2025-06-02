f4c3716267b292c218dfb12dc8ca257405657946
hayyazk <130911777+hayyazk@users.noreply.github.com>
2025-06-02 14:16:08 +0700

    feat: setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app bg-neutral-900 text-white min-h-screen p-4">
+      <h1 className="text-3xl font-bold mb-2 text-center">Statlistrik</h1>
+      <p className="text-amber-100 font-bold mb-4 space-x-7">Dashboard Pembangkitan dan Penggunaan Listrik Indonesia</p>
+    </div>
+  );
+}
+
+export default App;
