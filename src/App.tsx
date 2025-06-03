0f3fdd01b7cfe9261702244e755a7de1b8bf7219
Enderageous <13520012@std.stei.itb.ac.id>
2025-06-03 09:52:46 +0700

    province info

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Map from "./components/Map";
+import Pie from "./components/Pie";
 import "./App.css";
 
 function App() {
+
+  const [province] = React.useState("DKI Jakarta") 
+
   return (
     <div className="app bg-neutral-900 text-white min-h-screen p-4">
       <h1 className="text-3xl font-bold mb-2 text-center">Statlistrik</h1>
       <p className="text-amber-100 font-bold mb-4 space-x-7">Dashboard Pembangkitan dan Penggunaan Listrik Indonesia</p>
       <div className="grid gap-6">
         <Map />
+        <Pie province={province} />
       </div>
     </div>
   );
---
+++ src/components/Pie.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Pie = ({province}) => {
+    return (
+        <h1 className="text-3xl font-bold mb-2 text-center">{province}</h1>
+    )
+}
+
+export default Pie
